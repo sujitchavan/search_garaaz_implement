43bcd75518872d9d8eebad31f8a54dad6a09a130
Sujit Chavan <sujit.chavan@hp.com>
2021-09-11 17:04:19 +0530

    added logic for filter data

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,16 +3,51 @@ const data = require("./data/Data.json");
 
 const Search = () => {
   const [user, setUser] = useState([]);
+  const [show, setShow] = useState([]);
 
   useEffect(() => {
     setUser(data);
   }, []);
 
+  const handleChange = (event) => {
+    let timer;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      let show = user.filter((val) => {
+        if (
+          val.address
+            .toLowerCase()
+            .includes(event.target.value.toLowerCase()) ||
+          val.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
+          val.pincode
+            .toLowerCase()
+            .includes(event.target.value.toLowerCase()) ||
+          val.items
+            .join(",")
+            .toLowerCase()
+            .includes(event.target.value.toLowerCase()) ||
+          val.id.includes(event.target.value)
+        ) {
+          return val;
+        }
+        return;
+      });
+      event.target.value.length > 0 ? setShow(show) : setShow([]);
+    }, 500);
+  };
+
   return (
     <div>
-      {user.map((val) => {
-        return <div>{val.name}</div>;
-      })}
+      <input
+        type="text"
+        placeholder="Search User by ID, Address, ..."
+        onChange={handleChange}
+        id="searchbar"
+      />
+      {show &&
+        show.map((val) => {
+          return <div>{val.name}</div>;
+        })}
     </div>
   );
 };
